6f693e64fba684fd54a28fc4a572664e6f14b02f
Gabor Molnar <gabormolnar92@gmail.com>
2017-03-29 22:15:48 +0200

    Update webpack.config.js, gulp.config.js, gulpfile.js

--- resources/web/gulpfile.js
+++ resources/web/gulpfile.js
@@ -24,6 +24,7 @@ var rename = require('gulp-rename');
 var cleanCss = require('gulp-clean-css');
 var googleWebFonts = require('gulp-google-webfonts');
 var modifyCssUrls = require('gulp-modify-css-urls');
+var named = require('vinyl-named');
 
 /**
  *  Gulp config
@@ -31,6 +32,8 @@ var modifyCssUrls = require('gulp-modify-css-urls');
 
 var path = require('./gulp.config').path;
 var fileName = require('./gulp.config').fileName;
+var webpackConfig = require('./webpack.config.js');
+
 
 /**
  * Gulp Task
@@ -38,16 +41,17 @@ var fileName = require('./gulp.config').fileName;
  * Compile app.scss to app.css
  */
 gulp.task('scss', function() {
-    gulp.src(path.scss.src)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.init())
-        .pipe(autoprefixer(['last 2 version', 'ie 10']))
-        .pipe(cleanCss())
-        .pipe(rename(fileName.scss))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.scss.dest))
-        .pipe(connect.reload());
+	return gulp.src(path.scss.src)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer(['last 2 version', 'ie 10']))
+		.pipe(cleanCss())
+		.pipe(rename(fileName.scss))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(path.scss.dest))
+		.pipe(connect.reload());
 });
+
 
 /**
  * Gulp Task
@@ -56,102 +60,64 @@ gulp.task('scss', function() {
  *
  */
 
-gulp.task('google-fonts', function () {
-    var config = require('./gulp.config').googleFontsConfig;
-    return gulp.src(path.googleFonts.src)
-        .pipe(googleWebFonts(config))
-        .pipe(gulp.dest(path.googleFonts.dest));
+gulp.task('fonts-clear',function () {
+	return gulp.src(path.googleFonts.rm, {read: false})
+		.pipe(rimraf());
+});
+
+gulp.task('google-fonts',['fonts-clear'], function () {
+	var config = require('./gulp.config').googleFontsConfig;
+	return gulp.src(path.googleFonts.src)
+		.pipe(googleWebFonts(config))
+		.pipe(gulp.dest(path.googleFonts.dest));
 });
 
 gulp.task('fonts-url-fix',['google-fonts'],function () {
-    return gulp.src('./src/scss/base/_fonts.scss')
-        .pipe(modifyCssUrls({
-            modify: function (url, filePath) {
-                return url;
-            },
-            prepend: '../'
-        }))
-        .pipe(gulp.dest('./src/scss/base/'));
+	return gulp.src('./src/scss/base/_fonts.scss')
+		.pipe(modifyCssUrls({
+			modify: function (url, filePath) {
+				return url;
+			},
+			prepend: '../'
+		}))
+		.pipe(gulp.dest('./src/scss/base/'));
 });
 
-gulp.task('fonts',['google-fonts','fonts-url-fix']);
+gulp.task('fonts',['fonts-url-fix']);
 
 /**
  * Gulp Task
  *
- * Get all the dependency js and concat them in vendor.js
- */
-gulp.task('vendor', function (callback) {
-    gulp.src([
-            // JQuery and plugins
-            './bower_components/jquery/dist/jquery.js',
-
-            // jQuery UI components
-            // './bower_components/jquery.ui/ui/datepicker.js',
-            // './bower_components/jquery.ui/ui/core.js',
-            // './bower_components/jquery.ui/ui/widget.js',
-            // './bower_components/jquery.ui/ui/mouse.js',
-            // './bower_components/jquery.ui/ui/position.js',
-            // './bower_components/jquery.ui/ui/menu.js',
-            // './bower_components/jquery.ui/ui/autocomplete.js',
-            // './bower_components/jquery.ui/ui/slider.js',
-
-            // Bootstrap components
-            // './bower_components/bootstrap-js-components/dist/alert.js',
-            // './bower_components/bootstrap-js-components/dist/collapse.js',
-            // './bower_components/bootstrap-js-components/dist/dropdown.js',
-            // './bower_components/bootstrap-js-components/dist/transition.js',
-            // './bower_components/bootstrap-js-components/dist/modal.js',
-            // './bower_components/bootstrap-js-components/dist/tab.js',
-            // './bower_components/bootstrap-js-components/dist/tooltip.js',
-            // './bower_components/bootstrap-js-components/dist/popover.js',
-            // './bower_components/bootstrap-js-components/dist/scrollspy.js',
-            // './bower_components/bootstrap-js-components/dist/dropdown.js',
-            // './bower_components/bootstrap-js-components/dist/affix.js',
-
-        ])
-        .pipe(concat({path: 'vendor.js'}))
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename(fileName.vendor))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.vendor.dest));
-});
-
-/**
- * Gulp Task
- *
- * Generate dist/scripts/app.js from src/js/app.js
+ * Generate app.js
  */
 gulp.task('webpack', function () {
-    var config = require('./webpack.config.js');
-    return gulp.src(path.webpack.src)
-        .pipe(webpack(config))
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename(fileName.webpack))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.webpack.dest));
+	return gulp.src([
+		path.webpack.vendor,
+		path.webpack.app
+	])
+		.pipe(named())
+		.pipe(webpack(webpackConfig))
+		.pipe(gulp.dest(path.webpack.dest));
 });
 
 /**
  * Gulp Tasks
  *
- * Remove images from dist/images
- * Optimize images in src/images and copy them to dist/images
+ * Remove images from dist
+ * Optimize images and copy them to dist
  */
 gulp.task('images-optimize', function () {
-    return gulp.src(path.image.src)
-        .pipe(imagemin())
-        .pipe(gulp.dest(path.image.dest));
+	return gulp.src(path.image.src)
+		.pipe(imagemin())
+		.pipe(gulp.dest(path.image.dest));
 });
 
 gulp.task('images-remove', function () {
-    return gulp.src(path.image.rm, {read: false})
-        .pipe(rimraf());
+	return gulp.src(path.image.rm, {read: false})
+		.pipe(rimraf());
 });
 
-gulp.task('images', ['images-optimize']);
+gulp.task('images', ['images-remove', 'images-optimize']);
 
 /**
  * Gulp Task
@@ -159,20 +125,19 @@ gulp.task('images', ['images-optimize']);
  * Compile pug to html
  */
 gulp.task('pug', function () {
-    gulp.src([path.pug.src])
-        .pipe(pug({
-            pretty: false
-        }))
-        .pipe(prettify({
-            unformatted: ['pre', 'code'],
-            indent_inner_html: true,
-            indegulnt_size: 2,
-            brace_style: 'expand'
-        }))
-        .pipe(gulp.dest(path.pug.dest))
-        .pipe(connect.reload());
+	gulp.src([path.pug.src])
+		.pipe(pug({
+			pretty: false
+		}))
+		.pipe(prettify({
+			unformatted: ['pre', 'code'],
+			indent_inner_html: true,
+			indegulnt_size: 2,
+			brace_style: 'expand'
+		}))
+		.pipe(gulp.dest(path.pug.dest))
+		.pipe(connect.reload());
 });
-
 
 /**
  * Gulp Server
@@ -180,11 +145,11 @@ gulp.task('pug', function () {
  * Start a server for testing your app.
  */
 gulp.task('webserver', function() {
-    connect.server({
-        root: path.webserver.root,
-        livereload: true,
-        port: 8001
-    });
+	connect.server({
+		root: path.webserver.root,
+		livereload: true,
+		port: 8001
+	});
 });
 
 /**
@@ -193,10 +158,11 @@ gulp.task('webserver', function() {
  *  Watch all gulp tasks.
  */
 gulp.task('watch', function () {
-    gulp.watch(path.watch.webpack, ['webpack']);
-    gulp.watch(path.watch.pug, ['pug']);
-    gulp.watch(path.watch.scss, ['scss']);
-    gulp.watch(path.watch.images, ['images']);
+	gulp.watch(path.watch.webpack, ['webpack']);
+	gulp.watch(path.watch.pug, ['pug']);
+	gulp.watch(path.watch.scss, ['scss']);
+	gulp.watch(path.watch.less, ['less']);
+	gulp.watch(path.watch.images, ['images']);
 });
 
 /**
@@ -204,6 +170,6 @@ gulp.task('watch', function () {
  *
  * Initialize all tasks and watchers.
  */
-gulp.task('init', ['pug', 'scss', 'vendor', 'webpack', 'images','watch', 'webserver']);
+gulp.task('init-scss', ['pug', 'scss', 'webpack', 'images','watch', 'webserver']);
 
 gulp.task('default', ['scss','vendor','webpack']);
